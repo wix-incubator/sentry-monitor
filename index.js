8291a26c7555ecdbe5f5a688a3d477889c3cb478
Aaron Greenwald <aarong@wix.com>
2016-12-15 10:17:51 +0200

    refactor/cleanup

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const parse = require('parse-link-header');
@@ -7,9 +6,9 @@ const {authorization, org, project, searchTerms} = require('./config');
 const HOUR = 3600 * 1000;
 const BASE_URL = 'https://sentry.io/api/0/';
 const fetchOptions = {
-    headers: {
-        Authorization: authorization
-    }
+  headers: {
+    Authorization: authorization
+  }
 };
 
 // const getData = query => fetch(`${BASE_URL}projects/${org}/${project}/issues/?query=is:unresolved%20${query}&statsPeriod=`, fetchOptions)
@@ -46,54 +45,71 @@ const fetchOptions = {
 //         processData(data)
 //     })    
 
-const getPage = url => {
-    return fetch(url, fetchOptions)        
-        .then(res => {
-            const {next} = parse(res.headers.get('link'));
-            return res.json().then(data => ({
-                next: next.results === 'true' ? next.url : null,
-                data
-            })                        
-        )
-    })
-}
+const getPage = url => fetch(url, fetchOptions)
+  .then(res => res.json()
+    .then(data => {
+      if (!res.ok) {
+        throw new Error(`Status ${res.status} on ${url}: ${data.detail}`);
+      }
+      const {next} = parse(res.headers.get('link'));
+      return {
+        nextUrl: next.results === 'true' ? next.url : null,
+        data
+      };
+    }))
+  .catch(ex => {
+    console.error(ex);
+    throw ex;
+  });
 
-const getPagedData = (url, earliest, existing = []) => {
-    const max = 1000; //just in case
-    console.log('fetching page')
-    return getPage(url)    
-        .then(result => {
-            let {data} = result;
-            let stop;
-            if (new Date(data[data.length - 1].dateCreated) < earliest) {
-                data = _.filter(result.data, e => new Date(e.dateCreated) > earliest)
-                stop = true;
-            }
-            existing = [...existing, ...data]
-            if (result.next && existing.length < max && !stop) {
-                return getPagedData(result.next, earliest, existing)
-            } else {
-                return existing;
-            }
-                    
-        })    
-    }
+const PAGINATION_RECURSION_LIMIT = 1000; //just in case
+
+const getPagedData = (url, startDatetime, acc = [], datetimeField = 'dateCreated') => {
+  console.log('Fetching page...');
+  return getPage(url)
+    .then(result => {
+      let {data, nextUrl} = result;
+      let stop;
+      //TODO: is the assumption that data is order by the datetimeField desc correct?
+      if (new Date(data[data.length - 1][datetimeField]) < startDatetime) {
+          data = _.filter(result.data, item => new Date(item[datetimeField]) > startDatetime);
+          stop = true;
+      }
+      acc = [...acc, ...data];
+      if (nextUrl && acc.length < PAGINATION_RECURSION_LIMIT && !stop) {
+          return getPagedData(nextUrl, startDatetime, acc, datetimeField);
+      } else {
+          return acc;
+      }
+    })
+    .catch(ex => {
+      console.error('Failed to get page: ', url, ex);
+      throw ex;
+    });
+  };
 
 const endTime = new Date().getTime();
 const startTime = endTime - (HOUR / 12);
-const search = (e, searchTerms) => searchTerms.some(term => e.message.indexOf(term) !== -1);
-getPagedData(`${BASE_URL}projects/${org}/${project}/events/`, startTime).then(data => {
-    //get events in last five minutes, filter to interesting ones
-    console.log(`processing ${data.length} events`)
-    const events = _.filter(data, e => search(e, searchTerms))
-    const counts = _.countBy(events, 'groupID')
-    const mapped = Object.keys(counts).map(groupID => ({
-        groupID,    
-        count: counts[groupID], 
-        url: `https://sentry.io/${org}/${project}/issues/${groupID}/`,
-        message: _.find(events, e => e.groupID === groupID).message,
-    }))
-    console.info('Range: ', new Date(startTime), new Date(endTime))
-    console.info('Total Event Count:', events.length)
-    console.info('Issues', mapped)
-})
+const searchEventMessage = (event, searchTerms) => searchTerms.some(term => event.message.indexOf(term) !== -1);
+
+getPagedData(`${BASE_URL}projects/${org}/${project}/events/`, startTime, [], 'dateCreated').then(data => {
+  console.log(`Processing total of ${data.length} events in range`);
+  const events = _.filter(data, e => searchEventMessage(e, searchTerms));
+  const counts = _.countBy(events, 'groupID');
+  const mapped = Object.keys(counts).map(groupID => ({
+    groupID,
+    count: counts[groupID],
+    url: `https://sentry.io/${org}/${project}/issues/${groupID}/`,
+    message: _.find(events, e => e.groupID === groupID).message,
+  }));
+  const result = {
+    range: {
+      start: startTime,
+      end: endTime
+    },
+    totalEvents: events.length,
+    errors: mapped
+  };
+  console.info(result);
+  return result;
+});
