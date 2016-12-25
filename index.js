d452bace06e98407efdd639093c2dbd9637700fb
Aaron Greenwald <aarong@wix.com>
2016-12-25 15:38:01 +0200

    support authorization

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const app = require('express')();
 const run = require('./sentry-monitor');
 
 module.exports = function(config) {
+
+  app.use((req, res, next) => {
+    if (config.WSM_AUTH_KEY && req.headers['x-wsm_auth_key'] !== config.WSM_AUTH_KEY) {
+      console.log(`Request denied from ${req.ip}`, req.headers);
+      return res.sendStatus(403);
+    }
+    next();
+  });
+
   app.route('/')
     .post((req, res) => {
       run({debug: req.query.debug, config});
